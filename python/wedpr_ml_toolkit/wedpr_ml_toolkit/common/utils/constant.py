77c138f6b91dc40259edef6e78b837efb04e9670
cyjseagull <yujiechen_hust@163.com>
2024-11-22 16:30:44 +0800

    fix wedpr_ml_toolkit (#98)
    * fix wedpr_ml_toolkit
    
    * upgrade wedpr_ml_toolkit
    
    * remove pyproject.toml

--- python/wedpr_ml_toolkit/wedpr_ml_toolkit/common/utils/constant.py
+++ python/wedpr_ml_toolkit/wedpr_ml_toolkit/common/utils/constant.py
@@ -3,7 +3,7 @@
 class Constant:
     NUMERIC_ARRAY = [i for i in range(10)]
     HTTP_STATUS_OK = 200
-    WEDPR_API_PREFIX = '/api/wedpr/v3/'
+    WEDPR_API_PREFIX = 'api/wedpr/v3/'
     DEFAULT_SUBMIT_JOB_URI = f'{WEDPR_API_PREFIX}project/submitJob'
     DEFAULT_QUERY_JOB_STATUS_URL = f'{WEDPR_API_PREFIX}project/queryJobByCondition'
     DEFAULT_QUERY_JOB_DETAIL_URL = f'{WEDPR_API_PREFIX}scheduler/queryJobDetail'
