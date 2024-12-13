8b083fc283167f3f849fd62011d6c7140e496333
ywy2090 <912554887@qq.com>
2024-12-13 15:00:04 +0800

    fix connected_external_ip config bug (#126)

--- cpp/wedpr-helper/ppc-tools/src/config/PPCConfig.cpp
+++ cpp/wedpr-helper/ppc-tools/src/config/PPCConfig.cpp
@@ -668,7 +668,7 @@ void PPCConfig::loadMPCConfig(boost::property_tree::ptree const& _pt)
     m_mpcConfig.readPerBatchLines = _pt.get<std::uint64_t>("mpc.read_per_batch_lines", 100000);
     m_mpcConfig.spdzConnectedPort = _pt.get<int>("spdz.connected_port", 5894);
     
-    m_mpcConfig.spdzConnectedIP = _pt.get<std::string>("transport.connected_external_ip", "");
+    m_mpcConfig.spdzConnectedIP = _pt.get<std::string>("spdz.connected_external_ip", "");
     if (m_mpcConfig.spdzConnectedIP.empty()) {
         m_mpcConfig.spdzConnectedIP = _pt.get<std::string>("transport.host_ip", "");
     }
